88cb884cd425b2e9cd21290ab954efa194b6abcc
Iane Martins <ianemartins99@gmail.com>
2021-07-18 18:29:43 -0300

    Adicionados documento da landing page feita para o desafio 1 do hiring coders

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const formjs = document.getElementById('formjs')
+
+formjs.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+    
+})
